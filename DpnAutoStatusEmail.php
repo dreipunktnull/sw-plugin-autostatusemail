a90220749ecc7c5b2bc047d49ca6d9031c542e61
Björn Fromme <mail@bjo3rn.com>
2018-11-21 11:27:26 +0100

    Flush caches on plugin update

--- DpnAutoStatusEmail.php
+++ DpnAutoStatusEmail.php
@@ -4,7 +4,9 @@ namespace DpnAutoStatusEmail;
 
 use Shopware\Components\Plugin;
 use Shopware\Components\Plugin\Context\ActivateContext;
+use Shopware\Components\Plugin\Context\DeactivateContext;
 use Shopware\Components\Plugin\Context\InstallContext;
+use Shopware\Components\Plugin\Context\UpdateContext;
 
 class DpnAutoStatusEmail extends Plugin
 {
@@ -13,7 +15,23 @@ class DpnAutoStatusEmail extends Plugin
      */
     public function activate(ActivateContext $context)
     {
-        $context->scheduleClearCache(InstallContext::CACHE_LIST_ALL);
+        $context->scheduleClearCache(InstallContext::CACHE_LIST_DEFAULT);
+    }
+
+    /**
+     * @param UpdateContext $context
+     */
+    public function update(UpdateContext $context)
+    {
+        $context->scheduleClearCache(InstallContext::CACHE_LIST_DEFAULT);
+    }
+
+    /**
+     * @param DeactivateContext $context
+     */
+    public function deactivate(DeactivateContext $context)
+    {
+        $context->scheduleClearCache(InstallContext::CACHE_LIST_DEFAULT);
     }
 }
 
